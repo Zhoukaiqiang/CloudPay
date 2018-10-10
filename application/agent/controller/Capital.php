13db2dcffb31d56907bc51022f075aba875cfccf
周凯强 <490237797@qq.com>
2018-10-10 09:11:30 +0800

    更正代理商后台佣金

--- application/agent/controller/Capital.php
+++ application/agent/controller/Capital.php
@@ -28,23 +28,44 @@ class Capital extends Controller
             ['cloud_total_agent c', 'a.agent_id = c.id'],        //子代理商
             ['cloud_order d', 'd.merchant_id = a.id']            //订单表
         ];
-//        $agent_id = 3;
-        /** 检查是几级代理商 */
-        $check_level = TotalAgent::get($agent_id)->field("parent_id")->find();
 
-        if ($check_level->getData()["parent_id"] == 0) {
+        /** 检查是几级代理商 */
+//        $check_level = TotalAgent::get($agent_id)->field("parent_id")->find();
+        //mark
+
+//        $check_level->getData()["parent_id"] == 0
+        if (1) {
             /** 一级代理商 */
-            $data['list'] = TotalMerchant::alias('a')
-                ->field(['a.id,a.name,b.partner_name,a.merchant_rate,c.agent_name,c.agent_rate,b.rate,b.proportion,b.model'])
+            $rows = TotalMerchant::alias("a")
                 ->join($join)
-                ->where('a.agent_id', $agent_id)
+                ->where([
+                    "a.agent_id" => $agent_id,     //属于当前代理商的 商户
+                ])
                 ->group('a.id')
+                ->count("a.id");
+
+            $pages = page($rows);
+
+            $data['list'] = TotalMerchant::alias('a')
+                ->field(['a.id, a.name, b.partner_name, a.partner_id ,b.commission as partner_money, a.merchant_rate,c.agent_name,c.agent_rate,b.rate,b.proportion,b.model'])
+                ->join($join)
+                ->where([
+                    "a.agent_id" => $agent_id,     //属于当前代理商的 商户
+                    //"b.agent_id" => $agent_id,     //属于当前代理商的 合伙人
+                ])
+                ->group('a.id')
+                ->limit($pages["limit"], $pages["offset"])
                 ->select();
 
             if (!count($data['list'])) {
                 return_msg(400, "no data");
             }
 
+
+            $data["count"]["total_money"] = 0;
+            $data["count"]["child_money"] = 0;
+            $data["count"]["partner_money"] = 0;
+            $data["count"]["deserve_money"] = 0;
             //取出商户支付宝和微信交易额交易量
             foreach ($data['list'] as &$v) {
                 $where = [
@@ -53,7 +74,7 @@ class Capital extends Controller
                     'merchant_id' => $v['id']
                 ];
                 $alipay = Order::where($where)->sum('received_money');
-                $alipay_number = Order::where($where)->count();
+                $alipay_number = Order::where($where)->count('id');
                 $where1 = [
                     'status' => 1,
                     'pay_type' => 'wxpay',
@@ -64,68 +85,134 @@ class Capital extends Controller
                 //取出微信交易量
                 $wxpay_number = Order::where($where1)->count();
                 //计算佣金
-                $money = $alipay + $wxpay;//总交易量
-                //间联预估佣金=本代理商所负责商户的交易额*（二级代理费率-一级代理费率-平台与代理商约定的费率）
-                $v['total_money'] = $money * (($v['merchant_rate'] - $v['agent_rate']) / 100);//总佣金
-                $v['agent_money'] = $money * $v['merchant_rate'] / 100;//代理商佣金
-                if ($v['model'] == 0) {
-                    //安比例
-                    $v['partner_money'] = $money * $v['proportion'] / 100;
-                } elseif ($v['model'] == 1) {
-                    //按费率
-                    $v['partner_money'] = $money * ($v['merchant_rate']-$v['rate']) / 100;
+                $money = $alipay + $wxpay;//总交易额
+
+                //间联预估佣金 = 本代理商所负责商户的交易额*（二级代理费率-一级代理费率-平台与代理商约定的费率）
+
+
+                /** 如果有子代理商 */
+                if (!empty($v["agent_name"])) {
+
+                    $v["child_money"] = $money * (($v["merchant_rate"] - $v["agent_rate"]) / 100);    //子代理商佣金
+                } else {
+                    $v["child_money"] = 0;
                 }
+//                $v['agent_money'] = $money * $v['merchant_rate'] / 100;  //
+                /** 合伙人佣金 如果有合伙人*/
+                if (!empty($v["partner_id"])) {
+                    if ($v['model'] == 0) {
+                        //安比例
+                        $v['partner_money'] = $money * $v['proportion'] / 100;
+                    } elseif ($v['model'] == 1) {
+                        //按费率
+                        $v['partner_money'] = $money * ($v['merchant_rate'] - $v['rate']) / 100;
+                    }
+                } else {
+                    $v["partner_money"] = 0;
+                }
+                $v['total_money'] = $v["child_money"] + $v["partner_money"];
+                $v["deserve_money"] = $money * ($v["merchant_rate"] - $v["agent_rate"]) / 100;
                 $v['alipay'] = $alipay;
                 $v['alipay_number'] = $alipay_number;
                 $v['wxpay'] = $wxpay;
                 $v['wxpay_number'] = $wxpay_number;
+                $data["count"]["total_money"] += $v['total_money'];
+                $data["count"]["deserve_money"] += $v['deserve_money'];
+                $data["count"]["child_money"] += $v['child_money'];
+                $data["count"]["partner_money"] += $v['partner_money'];
             }
-
-        }else {
-            /** 二级代理商 */
-            return 2;
+            $data["pages"] = $pages;
+            $data["pages"]['rows'] = $rows;
         }
-
+        /** 获取结算统计数据 */
+        //mark
+        $data["index_count"]["pre_settle"] = $this->check_apply_money_by_month();
+        $data["index_count"]["settled_num"] =  TotalCapital::where(["agent_id" => $agent_id, "status" => 0])
+            ->whereTime("settlement_time" , ">", date("Y-1-1", time()))
+            ->count("id");
+        $data["index_count"]["pre_settle_num"] = (int)date("m", time()) - $data["index_count"]["settled_num"] ;
+        $data["index_count"]["settled"] = TotalCapital::where(["agent_id" => $agent_id, "status" => 0])
+            ->whereTime("settlement_time" , ">", date("Y-1-1", time()))
+            ->sum("settlement_money");
+//        else {
+//            /** 二级代理商 */
+//            $data["list"] = TotalMerchant::alias("mer")
+//                ->join("cloud_agent_partner ap", "ap.id = mer.partner_id")
+//                ->where([
+//                    "mer.agent_id" => $agent_id,  //属于当前代理商的 商户
+//                    "ap.agent_id" => $agent_id,     //属于当前代理商的 合伙人
+//                ])
+//                ->field("mer.id, mer.name, mer.agent_name, ap.partner_name,ap.proportion, ap.rate,ap.model")
+//
+//                ->select();
+//
+//            return_msg(200, 'success', $data);
+//
+//        }
         return_msg(200, 'success', $data);
     }
 
     /**
      * 时间查询
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
      */
     public function agent_time()
     {
         //获取时间
-        $time=request()->param('time');
+        $time = request()->param('time');
+
+        if (empty($time)) {
+            $time_flag = ">";
+            $time = 0;
+        }else {
+            $time_flag = "between";
+            $firstday = date('Y-m-01', $time);
+            $lastday =  date('Y-m-d', strtotime($firstday . ' +1 month -1 day'));
+            $time = [$firstday, $lastday];
+        }
+
         //获取当前代理商id
         $agent_id = Session::get("username_")['id'];
-        $agent_id=1;
+
         $join = [
             ['cloud_agent_partner b', 'a.partner_id=b.id'],
             ['cloud_total_agent c', 'a.agent_id=c.id'],
             ['cloud_order d', 'd.merchant_id=a.id']
         ];
 
-        //获取总行数
-//        $rows = TotalMerchant::alias('a')
-//            ->join($join)
-//            ->where('a.agent_id', $agent_id)
-//            ->group('a.id')
-//            ->count();
-//        //分页
-//        $pages = page($rows);
-        $data['list'] = TotalMerchant::alias('a')
-            ->field(['a.id,a.name,b.partner_name,a.merchant_rate,c.agent_name,c.agent_rate,b.rate,b.proportion,b.model'])
+        //mark
+        /** 一级代理商 */
+        $rows = TotalMerchant::alias("a")
+            ->whereTime("pay_time", $time_flag , $time)
             ->join($join)
-            ->where('a.agent_id', $agent_id)
-            ->whereTime('pay_time','>',$time)
+            ->where([
+                "a.agent_id" => $agent_id,     //属于当前代理商的 商户
+            ])
+
             ->group('a.id')
-//            ->limit($pages['offset'], $pages['limit'])
+            ->count("a.id");
+
+        $pages = page($rows);
+
+        $data['list'] = TotalMerchant::alias('a')
+            ->field(['a.id, a.name, b.partner_name, a.partner_id ,b.commission as partner_money, a.merchant_rate,c.agent_name,c.agent_rate,b.rate,b.proportion,b.model'])
+            ->whereTime("pay_time", $time_flag , $time)
+            ->join($join)
+            ->where([
+                "a.agent_id" => $agent_id,     //属于当前代理商的 商户
+            ])
+
+            ->group('a.id')
+            ->limit($pages["limit"], $pages["offset"])
             ->select();
+
 
         if (!count($data['list'])) {
             return_msg(400, "no data");
         }
-//        dump($data);die;
+
         //取出商户支付宝和微信交易额交易量
         foreach ($data['list'] as &$v) {
             $where = [
@@ -134,7 +221,7 @@ class Capital extends Controller
                 'merchant_id' => $v['id']
             ];
             $alipay = Order::where($where)->sum('received_money');
-            $alipay_number = Order::where($where)->count();
+            $alipay_number = Order::where($where)->count('id');
             $where1 = [
                 'status' => 1,
                 'pay_type' => 'wxpay',
@@ -145,27 +232,45 @@ class Capital extends Controller
             //取出微信交易量
             $wxpay_number = Order::where($where1)->count();
             //计算佣金
-            $money = $alipay + $wxpay;//总交易量
-            //间联预估佣金=本代理商所负责商户的交易额*（二级代理费率-一级代理费率-平台与代理商约定的费率）
-            $v['total_money'] = $money * (($v['merchant_rate'] - $v['agent_rate']) / 100);//所得佣金
-            $v['agent_money'] = $money * $v['merchant_rate'] / 100;//代理商佣金
-            if ($v['model'] == 0) {
-                //安比例
-                $v['partner_money'] = $money * $v['proportion'] / 100;
-            } elseif ($v['model'] == 1) {
-                //按费率
-                $v['partner_money'] = $money * ($v['merchant_rate']-$v['rate']) / 100;
+            $money = $alipay + $wxpay;//总交易额
+
+            //间联预估佣金 = 本代理商所负责商户的交易额*（二级代理费率-一级代理费率-平台与代理商约定的费率）
+
+
+            /** 如果有子代理商 */
+            if (!empty($v["agent_name"])) {
+
+                $v["child_money"] = $money * (($v["merchant_rate"] - $v["agent_rate"]) / 100);    //子代理商佣金
+            } else {
+                $v["child_money"] = 0;
             }
+//                $v['agent_money'] = $money * $v['merchant_rate'] / 100;  //
+            /** 合伙人佣金 如果有合伙人*/
+            if (!empty($v["partner_id"])) {
+                if ($v['model'] == 0) {
+                    //安比例
+                    $v['partner_money'] = $money * $v['proportion'] / 100;
+                } elseif ($v['model'] == 1) {
+                    //按费率
+                    $v['partner_money'] = $money * ($v['merchant_rate'] - $v['rate']) / 100;
+                }
+            } else {
+                $v["partner_money"] = 0;
+            }
+            $v['total_money'] = $v["child_money"] + $v["partner_money"];
+            $v["deserve_money"] = $money * ($v["merchant_rate"] - $v["agent_rate"]) / 100;
             $v['alipay'] = $alipay;
             $v['alipay_number'] = $alipay_number;
             $v['wxpay'] = $wxpay;
             $v['wxpay_number'] = $wxpay_number;
         }
-
+        $data["pages"] = $pages;
+        $data["pages"]['rows'] = $rows;
 
 
         return_msg(200, 'success', $data);
     }
+
     /**
      * 代理商申请结算
      * @param [int] agent_id  代理商id 从session获取
@@ -176,7 +281,7 @@ class Capital extends Controller
     public function apply_money(Request $request)
     {
 
-        $agent_id =  Session::get("username_")['id'];
+        $agent_id = Session::get("username_")['id'];
         /**  agent_id不存在返回 */
         if (!$agent_id) {
             return_msg(400, 'id不存在');
@@ -211,10 +316,10 @@ class Capital extends Controller
                     //$check_send = $this->send_msg_to_phone(18670138762,'申请');
 
                     /** 检查是否发送成功 *//*$check_send*/
-                    if(0 ) {
+                    if (0) {
                         return_msg(200, '申请成功');
-                    }else {
-                        return_msg(400 , '短信发送失败');
+                    } else {
+                        return_msg(400, '短信发送失败');
                     }
 
                 } else {
@@ -247,25 +352,30 @@ class Capital extends Controller
      * @throws \think\exception\DbException
      * @return float total 总额
      */
-    public function check_apply_money_by_month($time) {
+    public function check_apply_money_by_month($time)
+    {
 
         /** 时间区间 */
-        $id =  Session::get("username_")['id'];
+        $id = Session::get("username_")['id'];
         $res = TotalAgent::get($id)->field('parent_id')->find();
-        $merchant_total = [];$agent_id_arr = []; $sum =0;$agent_sec_rate_arr = [];$per_sec_agent_merchant_id=[];
+        $merchant_total = [];
+        $agent_id_arr = [];
+        $sum = 0;
+        $agent_sec_rate_arr = [];
+        $per_sec_agent_merchant_id = [];
         /** 代理商费率 $agent_rate */
         $agent_rate = (float)TotalAgent::get($id)->field('agent_rate')->find()->toArray()['agent_rate'];
 
         /** 所有直接下属商户总交易额 $merchant_total */
         $result = TotalMerchant::field('m.merchant_rate, o.received_money')
-            ->alias('m')->join('cloud_order o','m.id = o.merchant_id')
+            ->alias('m')->join('cloud_order o', 'm.id = o.merchant_id')
             ->where([
-                "m.agent_id"  => $id,
+                "m.agent_id" => $id,
             ])
 //            ->whereTime('pay_time', 'between', [$time])
             ->select();
-        foreach($result as $v) {
-            array_push($merchant_total, ( (float)$v['merchant_rate'] - $agent_rate )/100 * (float)$v['received_money'] );
+        foreach ($result as $v) {
+            array_push($merchant_total, ((float)$v['merchant_rate'] - $agent_rate) / 100 * (float)$v['received_money']);
         }
 
         /** 如果存在parent_id为一级代理否则为二级代理 */
@@ -281,28 +391,29 @@ class Capital extends Controller
 
             foreach ($agent_id_arr as $agent_id) {
                 $per_sec_agent_merchant_id[] = TotalMerchant::field("m.id")->alias("m")
-                    ->where("m.agent_id","eq",$agent_id)
+                    ->where("m.agent_id", "eq", $agent_id)
                     ->select();
             }
 
 
             $sec_merchant_total = [];
             foreach ($per_sec_agent_merchant_id as $v) {
-                foreach($v as $vv) {
+                foreach ($v as $vv) {
                     /** 二级代理商a的总交易额 */
-                    $sec_merchant_total[] = Order::where("merchant_id",'eq',$vv['id'])->sum("received_money");
+                    $sec_merchant_total[] = Order::where("merchant_id", 'eq', $vv['id'])->sum("received_money");
                 }
             }
-            for ($i =0; $i< count($agent_id_arr); $i++) {
-                $sum += ($agent_sec_rate_arr[$i] - $agent_rate[$i] ) * $sec_merchant_total[$i];
+            for ($i = 0; $i < count($agent_id_arr); $i++) {
+                $sum += ($agent_sec_rate_arr[$i] - $agent_rate[$i]) * $sec_merchant_total[$i];
             }
 
             return array_sum($merchant_total) + $sum;
 
-        }else {
+        } else {
             return array_sum($merchant_total);
         }
     }
+
     /**
      * 检验当前代理商是否已经申请过
      * @param $id 当前代理商id
@@ -327,7 +438,7 @@ class Capital extends Controller
      * @param [int] $now 当前的时间戳
      * @return string/bool  检测结果
      */
-    protected function apply_rules($now,$id)
+    protected function apply_rules($now, $id)
     {
         $date['year'] = intval(date('Y', $now));
         $date['month'] = intval(date('m', $now));
@@ -341,7 +452,7 @@ class Capital extends Controller
         } else {
             /** 移除限制 */
             $res = TotalAgent::update(["limit" => 1], ["id" => $id]);
-            if($res['limit'] !== 1) {
+            if ($res['limit'] !== 1) {
                 return_msg(400, '操作失败');
             }
             return_msg(400, '申请失败，请在每月27日至下月10日之间申请。');
@@ -355,7 +466,7 @@ class Capital extends Controller
      * @param $phone
      * @param $msg
      */
-    public function send_msg_to_phone($phone, $msg='')
+    public function send_msg_to_phone($phone, $msg = '')
     {
         $curl = curl_init();
 
@@ -367,9 +478,9 @@ class Capital extends Controller
         //配置submail
         $data = [
             'appid' => '27075', //应用id
-            'to'  => $phone,     //要接受短信的电话
+            'to' => $phone,     //要接受短信的电话
             'project' => 'Jaayb', //模板标识
-            'vars'  => '',
+            'vars' => '',
             'signature' => '5ac305ef38fb126d2a0ec5304040ab7d', //应用签名
         ];
 
@@ -377,13 +488,14 @@ class Capital extends Controller
         $res = curl_exec($curl);
         curl_close($curl);
         $res = json_decode($res);
-        if($res->status !== 'success') {
+        if ($res->status !== 'success') {
             return false;
-        }else {
+        } else {
             return true;
         }
 
     }
+
     /**
      * 子代佣金统计
      *
