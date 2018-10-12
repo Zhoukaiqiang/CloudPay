1454633fa122d7a19147cc511408df11633a9a46
lishangwanglai0 <Guo_Yang_0@163.com>
2018-10-12 09:05:25 +0800

    更新

--- application/common.php
+++ application/common.php
@@ -314,12 +314,14 @@ if (!function_exists('sign_ature')) {
         if ($ids == 0000) {
             $data = ['serviceId', 'version', 'incom_type', 'stl_typ', 'stl_sign', 'stl_oac', 'bnk_acnm', 'wc_lbnk_no', 'bus_lic_no', 'bse_lice_nm', 'crp_nm', 'mercAdds', 'bus_exp_dt', 'crp_id_no', 'crp_exp_dt', 'stoe_nm', 'stoe_cnt_nm', 'stoe_cnt_tel', 'mcc_cd', 'stoe_area_cod', 'stoe_adds', 'trm_rec', 'mailbox', 'alipay_flg', 'yhkpay_flg','mercId','orgNo','imgTyp','imgNm','log_no','stoe_id','lbnk_nm'];
             $stra = '';
+
             foreach($arr as $k=>$v){
                 if (in_array($k,$data)) {
 //                    echo $k."<br/>";
                     $stra .= $v;
                 }
             }
+//            var_dump($stra);die;
         } else if ($ids == 1111) {
             $data = ['check_flag', 'msg_cd', 'msg_dat', 'mercId', 'log_no', 'stoe_id', 'mobile', 'sign_stats', 'deliv_stats'];
             $stra = '';
---
+++ application/merchant/controller/ssss.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guoyang
+ * Date: 2018/10/11
+ * Time: 18:32
+ */
+
+namespace app\merchant\controller;
+
+
+class ssss
+{
+ public $data = ['serviceId', 'version', 'incom_type', 'stl_typ', 'stl_sign', 'stl_oac', 'bnk_acnm', 'wc_lbnk_no', 'bus_lic_no', 'bse_lice_nm', 'crp_nm', 'mercAdds', 'bus_exp_dt', 'crp_id_no', 'crp_exp_dt', 'stoe_nm', 'stoe_cnt_nm', 'stoe_cnt_tel', 'mcc_cd', 'stoe_area_cod', 'stoe_adds', 'trm_rec', 'mailbox', 'alipay_flg', 'yhkpay_flg','mercId','orgNo','imgTyp','imgNm','log_no','stoe_id','lbnk_nm'];
+
+}
