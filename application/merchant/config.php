37dd24ed4d3451dbeaaabd49180b487c9a27d7b5
周凯强 <490237797@qq.com>
2018-10-13 11:36:58 +0800

    添加app后台 配置文件

---
+++ application/merchant/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by KaiQiang-use by PhpStorm.
+ * User: fennu
+ * Date: 2018/10/13
+ * Time: 11:32
+ *
+ * ThinkPHP 模块配置表
+ */
+
+return [
+    'session' =>[
+        'expire' => 3,
+    ],
+];
