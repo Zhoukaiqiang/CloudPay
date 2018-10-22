f3d160eef426ad5728c25bf96753dde05ab089bb
周凯强 <490237797@qq.com>
2018-10-22 18:22:59 +0800

    增加Qrcode

--- public/index.php
+++ public/index.php
@@ -13,10 +13,10 @@
 
 // 定义应用目录
 
-//define("KEY", "7B7B64051113E3EB09725CBD69A55E79");
-define("KEY", "9773BCF5BAC01078C9479E67919157B8");
-//define('ORG_NO', "27573");
-define('ORG_NO', "518");
+define("KEY", "7B7B64051113E3EB09725CBD69A55E79");
+//define("KEY", "9773BCF5BAC01078C9479E67919157B8");
+define('ORG_NO', "27573");
+//define('ORG_NO', "518");
 
 define('APP_PATH', __DIR__ . '/../application/');
 
